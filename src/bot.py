864dc574cbe96761369f5139ad58264a861f30e7
Antony Martel <antony.martel.1@ulaval.ca>
2020-12-28 16:19:59 -0500

    Start bot

--- src/bot.py
+++ src/bot.py
@@ -1,9 +1,23 @@
+import image_map.py
 class Bot:
     def __init__(self, map, drone):
-        pass
+        self.map = map
+        self.drone = drone
+        self.validated_map = [False for _ in range(self.drone.size) for _ in range(self.drone.size)]
+        
 
     def get_map(self):
+        
         pass
+        
 
     def move(self):
+        #filtre des couleurs
+        
+
+
+        #Reconstruction
+        
+
+
         pass
