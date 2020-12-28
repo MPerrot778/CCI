a68a7d53b99a9cc1858596fca263e44d223b65f3
Mikaël Perrot <mik.perrot778@outlook.fr>
2020-12-28 14:57:24 -0500

    map_image update

--- Level 1/1A/map.py
+++ Level 1/1A/map.py
@@ -1,4 +1,3 @@
-
 class image_map:
 
     def __init__(self,inFile):
@@ -27,38 +26,71 @@ class image_map:
                 if(count == 1):
                     n = int(part_line[2])
                     max_count = n*n*n
-                    unscrambled_img = [[["" for k in range(n)] for j in range(n)] for i in range(n)]
-                    scrambled_img  = [[[""]*n]*n]*n
-                    print(max_count)
+                    unscrambled_img = [[[() for k in range(n)] for j in range(n)] for i in range(n)]
+                    scrambled_img  = [[[() for k in range(n)] for j in range(n)] for i in range(n)]
 
                 elif(count > 1 and count <= max_count):
-                    print(line)
-                    coords = tuple(part_line[0])
-                    unscrambled_img[int(coords[0])][int(coords[2])][int(coords[4])] = part_line[2].strip('\n')
+                    pixel = part_line[2].strip('"\n')
+                    coords = tuple(map(int,part_line[0].split(',')))
+
+                    if(pixel == ""):
+                        pixel_tuple = ()
+                    else:
+                        pixel_tuple = tuple(map(int,pixel.split('_')))
+
+                    unscrambled_img[coords[0]][coords[1]][coords[2]] = pixel_tuple
 
                 elif(count > max_count+4):
-                    print(line)
-                    coords = tuple(part_line[0])
-                    scrambled_img[int(coords[0])][int(coords[2])][int(coords[4])] = part_line[2].strip('\n')
-                count +=1
+                    pixel = part_line[2].strip('"\n')
+                    coords = tuple(map(int,part_line[0].split(',')))
+
+                    if(pixel == ""):
+                        pixel_tuple = ()
+                    else:
+                        pixel_tuple = tuple(map(int,pixel.split('_')))
+
+                    scrambled_img[coords[0]][coords[1]][coords[2]] = pixel_tuple
+
+                count += 1
         
         return unscrambled_img, scrambled_img
 
 
-    def get_ImageSize(self):
+    def get_imageSize(self):
         return(len(self.unscrambled))
+
+    # Scrambled image getter
     
-    def get_2DMap(self):
-        return(self.scrambled[1][0][:])
+    def get_2dScrambled(self,layer):
+        return(self.scrambled[layer])
+    
+    def get_3dScrambled(self):
+        return self.scrambled
+    
+    def get_pixelColorScrambled(self,coordinates):
+        return self.scrambled[coordinates[0]][coordinates[1]][coordinates[2]]
+    
+    # Unscrambled image getter
 
+    def get_2dUnscrambled(self,layer):
+        return(self.unscrambled[layer])
+    
+    def get_3dUnscrambled(self):
+        return self.unscrambled
+    
+    def get_pixelColorUnscrambled(self,coordinates):
+        return self.unscrambled[coordinates[0]][coordinates[1]][coordinates[2]]
+    
+    # Scrambled image setter
 
+    def set_pixelColorScrambled(self,coordinates,color):
+        self.scrambled[coordinates[0]][coordinates[1]][coordinates[2]] = color
+    
 
 def main():
-    map = image_map("hard.txt")
-
-    img2d = map.get_2DMap()
-
-    print(img2d)
+    map_easy = image_map("hard.txt")
+    img2d = map_easy.get_3dScrambled()
+    print(img2d[1][1])
 
 if __name__ == "__main__":
     main()
