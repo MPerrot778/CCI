e8c6e66615db35db8a3237c0ae08c8d89a3475e7
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2020-12-28 13:29:48 -0500

    Added methods to Drone and started using Map

--- Level 1/1A/drone.py
+++ Level 1/1A/drone.py
@@ -1,12 +1,15 @@
+import image_map
+
+EMPTY_COLOR = ""
+
 class Drone:
-    def __init__(self, game_map_3d, game_map_2d):
-        self.game_map_3d = game_map_3d
-        self.game_map_2d = game_map_2d
-        self.size = len(game_map_2d)
+    def __init__(self, game_map: image_map):
+        self.game_map = game_map
+        self.size = len(game_map.scrambled)
         self.hopper = []
-        self.memory = None
+        self.memory = ["" for _ in range(self.size) for _ in range(self.size) for _ in range(self.size)]
         # TODO: find initial position
-        self.position = (0, 0, self.size - 1)
+        self.position = (0, 0)
         self.last_touched_color = None
 
     def get_memory(self):
@@ -15,9 +18,24 @@ class Drone:
     def get_hopper(self):
         return self.hopper
 
-    def add_block_to_hopper(self, position, color):
+    def get_max_hopper_size(self):
+        return len(self.hopper) // 2
+
+    def add_block_to_hopper(self, color):
+        if self.get_max_hopper_size() >= self.size:
+            raise Exception("Hopper is full, can't add block to it!")
         self.hopper.append(color)
-        self.game_map_3d[position[0]][position[1]][position[2]] = None
+
+    def get_top_color(self):
+        color = EMPTY_COLOR
+        for z in range(self.size - 1, 0, -1):
+            if color != EMPTY_COLOR:
+                color = self.game_map.scrambled[self.position[0]][self.position[1]][z]
+                break
+        return color
+
+    def add_block_to_memory(self, position, color):
+        self.memory[position[0]][position[1]][position[2]] = color
 
     def move(self, delta) -> int:
         """
@@ -27,9 +45,19 @@ class Drone:
         """
         pass
 
-    def place(self, color) -> int:
-        if self.game_map_3d[self.position[0]][self.position[1]][self.size - 1] is not None:
-            raise Exception("Can't place a block there, already full")
+    def take(self) -> int:
+        for z in range(size - 1, 0, -1):
+            if self.game_map[self.position[0]][self.position[1]][z] is not None:
+                self.add_block_to_hopper(self.game_map.scrambled[self.position[0]][self.position[1]][z])
+                self.game_map.scrambled[position[0]][position[1]][z] = None
+
+    def place(self, color, altitude) -> int:
+        # validate the altitude is not under existing blocks
+        for z in range(self.size - 1, altitude, -1):
+            if self.game_map[self.position[0]][self.position[1]][z] is not None:
+                raise Exception("Can't place a block there, already full")
+
+        # find the block color in the hopper
         for i, c in enumerate(self.hopper):
             if c == color:
                 hopper_index = i
@@ -37,12 +65,8 @@ class Drone:
         else:
             raise Exception("Color not found in hopper")
 
-        for z in range(self.size - 1, 0, -1):
-            if self.game_map_3d[self.position[0]][self.position[1]][z] is not None:
-                self.game_map_3d[self.position[0]][self.position[1]][z + 1] = color
-                # TODO: update game_map_2d
-                del self.hopper[hopper_index]
-                break
+        self.game_map.scrambled[self.position[0]][self.position[1]][altitude] = color
+        del self.hopper[hopper_index]
 
         if self.last_touched_color == color:
             time_elapsed = 2
