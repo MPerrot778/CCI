022adb084364953c3460a6c66e3041df097ae5da
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2021-01-02 14:46:08 -0500

    Better parameter names in Bot

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,11 @@
 class Bot:
-    def __init__(self, map, drone):
+    def __init__(self, unscrambled_map, drone):
         pass
 
-    def get_map(self):
+    def get_game_map(self):
+        """
+        Returns the current Game map.
+        """
         pass
 
     def move(self):
