d6418c6a118d83c31940e9f7999d9b2699afa7e6
JustinHogue <justin.hogue.2@ulaval.ca>
2021-01-24 15:30:23 -0500

    Ajout de la fonction get_library_score qui renvoie le score d'une librairie.

--- Hash_code_book/Solver.py
+++ Hash_code_book/Solver.py
@@ -3,7 +3,7 @@ from Problem import *
 class Solver :
     def __init__(self, problem: Problem):
         self.problem = problem
-        self.books_read = {}
+        self.books_read = set()
         self.days_left = problem.D
         pass
 
@@ -14,4 +14,19 @@ class Solver :
         pass
 
     def get_library_score(self, library_id):
-        pass
+        score_de_la_librairie = 0
+        if self.days_left > self.problem.Libraries[library_id].T : #On s'assure que le nombre de jours pour le sign_up est plus petit que le nombre de jours restant
+            days_of_book_scanning_left:int = self.days_left - self.problem.Libraries[library_id].T
+            while days_of_book_scanning_left != 0: #Tant qu'il nous reste des journées de traitement, on continue
+                days_of_book_scanning_left -= 1
+                number_of_books_scanned_in_parallel = self.problem.Libraries[library_id].M
+                books_left_to_be_treated = self.problem.Libraries[library_id].books_sorted_by_score
+                while number_of_books_scanned_in_parallel != 0 and len(books_left_to_be_treated) != 0:
+                    id_of_book = books_left_to_be_treated[0]
+                    while id_of_book in self.books_read:
+                        books_left_to_be_treated.pop(0)
+                        id_of_book = books_left_to_be_treated[0]
+                    books_left_to_be_treated.pop(0)
+                    number_of_books_scanned_in_parallel -= 1
+                    score_de_la_librairie += self.problem.get_score(id_of_book)
+        return score_de_la_librairie
