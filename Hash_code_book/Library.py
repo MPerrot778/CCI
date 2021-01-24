14d0d1f359bece0e88230b5d24c92b1e668730b8
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2021-01-24 16:07:48 -0500

    Reverse list sort

--- Hash_code_book/Library.py
+++ Hash_code_book/Library.py
@@ -4,7 +4,7 @@ class Library :
         self.N = N # the number of books in library
         self.T = T # the number of days it takes to nish the library signup process for library
         self.M = M # the number of books that can be shipped from library j to the scanning facility per day, once the library is signed up.
-        self.books_sorted_by_score = sorted(books)
+        self.books_sorted_by_score = sorted(books, reverse=True)
 
 
     
