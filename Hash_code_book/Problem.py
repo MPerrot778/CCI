b9590173c89e1642b312a29317be70fc60a98efd
Mikaël Perrot <mik.perrot778@outlook.fr>
2021-01-24 15:58:09 -0500

    fixed problem with Problem

--- Hash_code_book/Problem.py
+++ Hash_code_book/Problem.py
@@ -1,11 +1,11 @@
-from Hash_code_book.Library import Library
-from Hash_code_book.Book import Book
+from Library import Library
+from Book import Book
 
 class Problem :
     def __init__(self,fileName):
         self.fileName = fileName
 
-        B,L,D,libraries = self.read()
+        B,L,D,libraries,scores = self.read()
 
         self.B = B
         self.L = L
@@ -14,33 +14,40 @@ class Problem :
 
     def read(self):
         # read file and extract attribute of the problem
+        B = 0
+        L = 0
+        D = 0
         with open(self.fileName,'r') as f :
             lines = f.readlines()
             row = 0
+            scores = []
             books_type =  []
             books= []
             lib_list = []
             for line in lines:
-                line = line.split(' ')
-                if row == 0:
-                    B = int(line[0])
-                    L = int(line[1])
-                    D = int(line[2])
-                elif row == 1:
-                    for i in range(B):
-                        books_type += [Book(i,int(line[i]))]
-                elif row > 1:
-                    if(row%2 == 0):
-                        N = int(line[0])
-                        T = int(line[1])
-                        M = int(line[2])
-                    else:
-                        for i in range(len(line)):
-                            books += [books_type[int(line[i])]]
-                        print(books)
-                        lib_list += [Library(N,T,M,books)]
+                if(line == '\n'):
+                    break
+                else :
+                    line = line.split(' ')
+                    if row == 0:
+                        B = int(line[0])
+                        L = int(line[1])
+                        D = int(line[2])
+                    elif row == 1:
+                        for i in range(B):
+                            books_type += [Book(i,int(line[i]))]
 
-                row += 1
+                    elif row > 1:
+                        if(int(row%2) == 0):
+                            N = int(line[0])
+                            T = int(line[1])
+                            M = int(line[2])
+                            books = []
+                        else:
+                            for i in range(len(line)):
+                                books += [books_type[int(line[i])]]
+                            lib_list += [Library(N,T,M,books)]
+                    row += 1
         return B,L,D,lib_list
 
     
