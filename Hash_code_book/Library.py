38ec0b354c75ef2b823766f976e8964e85e574b6
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2021-01-24 14:34:31 -0500

    Added sorted books in library

--- Hash_code_book/Library.py
+++ Hash_code_book/Library.py
@@ -1,7 +1,8 @@
 
 class Library : 
-    def __init__(self, N, T, M, books):
-        pass
+    def __init__(self, N, T, M, books, book_scores):
+        self.books_sorted_by_score = []
+        # TODO: add attributes
 
     
 
