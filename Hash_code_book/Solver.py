997c9204187b6c80486ae07b8bdebfb87bb575de
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2021-01-24 14:24:51 -0500

    Added get_library_score function signature

--- Hash_code_book/Solver.py
+++ Hash_code_book/Solver.py
@@ -1,6 +1,8 @@
 class Solver :
     def __init__(self,problem):
         self.problem = problem
+        self.books_read = {}
+        self.days_left = problem.D
         pass
 
     def get_solution(self):
@@ -9,3 +11,5 @@ class Solver :
     def write_solution(self, outName):
         pass
 
+    def get_library_score(self, library_id):
+        pass
