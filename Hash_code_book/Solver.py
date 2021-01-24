e16e768eb808499c92fe745cdbd32a4c4e99d799
JustinHogue <justin.hogue.2@ulaval.ca>
2021-01-24 15:55:55 -0500

    Ajout de commentaires dans la fonction get_library_score et correction de problématique de logique.

--- Hash_code_book/Solver.py
+++ Hash_code_book/Solver.py
@@ -15,12 +15,12 @@ class Solver :
 
     def get_library_score(self, library_id):
         score_de_la_librairie = 0
-        if self.days_left > self.problem.libraries[library_id].T : #On s'assure que le nombre de jours pour le sign_up est plus petit que le nombre de jours restant
-            days_of_book_scanning_left: int = self.days_left - self.problem.libraries[library_id].T
+        if self.days_left > self.problem.libraries[library_id].T: #On s'assure que le nombre de jours pour le sign_up est plus petit que le nombre de jours restant
+            days_of_book_scanning_left = self.days_left - self.problem.libraries[library_id].T
+            books_left_to_be_treated = self.problem.libraries[library_id].books_sorted_by_score
             while days_of_book_scanning_left != 0: #Tant qu'il nous reste des journées de traitement, on continue
                 days_of_book_scanning_left -= 1
                 number_of_books_scanned_in_parallel = self.problem.libraries[library_id].M
-                books_left_to_be_treated = self.problem.libraries[library_id].books_sorted_by_score
                 #Tant que le nombre de livres à traiter en parallèle et la taille de la liste des livres de la librairie ne sont pas de 0, on continue
                 while number_of_books_scanned_in_parallel != 0 and len(books_left_to_be_treated) != 0:
                     id_of_book = books_left_to_be_treated[0].book_id
