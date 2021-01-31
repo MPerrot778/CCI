851cdb54dbb619c42694ddbe4fb2ff0a20626551
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2021-01-31 15:42:01 -0500

    Fixed arm type signature

--- Hash_code_smartphone/Arm.py
+++ Hash_code_smartphone/Arm.py
@@ -4,20 +4,20 @@ import numpy as np
 
 
 class Arm:
-    def __init__(self, mounting_point: Tuple[int], used_map: np.ndarray):
+    def __init__(self, mounting_point: Tuple[int, int], used_map: np.ndarray):
         self.moves: List[str] = []
         self.mounting_point = mounting_point
         self.tail = deque([mounting_point])
         self.used_map = used_map
         self.actions = []
 
-    def move(self, goal_position: Tuple[int]):
+    def move(self, goal_position: Tuple[int, int]):
         pass
 
-    def get_next_position(self, goal_position: Tuple[int]) -> Tuple[int]:
+    def get_next_position(self, goal_position: Tuple[int, int]) -> Tuple[int, int]:
         pass
 
-    def get_distance(self, goal_position: Tuple[int]) -> int:
+    def get_distance(self, goal_position: Tuple[int, int]) -> int:
         pass
 
     def retract(self):
