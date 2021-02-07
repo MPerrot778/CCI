8d5f79cfc1aa52185127a0ed5c474b788e9f148d
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2021-02-07 16:25:27 -0500

    Solving all problems in main

--- Hash_code_self_driving/main.py
+++ Hash_code_self_driving/main.py
@@ -2,9 +2,19 @@ from Problem import Problem
 from Solver import Solver
 
 if __name__ == '__main__':
-    problem = Problem.get_from_file("Problems/b_should_be_easy.in")
-    solver = Solver(problem)
-    solver.solve()
-    print(solver.vehicule_rides)
-    solver.submit("out.txt")
-    print("score", solver.total_score)
+    problem_names = [
+        "a_example",
+        "b_should_be_easy",
+        "c_no_hurry",
+        "d_metropolis",
+        "e_high_bonus"
+    ]
+    for problem_name in problem_names:
+        problem = Problem.get_from_file(f"Problems/{problem_name}.in")
+        solver = Solver(problem)
+        solver.solve()
+
+        print(f"Problem {problem_name}")
+        print(solver.vehicule_rides)
+        print("score", solver.total_score)
+        solver.submit(f"Solutions/{problem_name}.out")
