bf430525604da69cf1fe9a03af0311e984f17d86
Mikaël Perrot <mik.perrot778@outlook.fr>
2021-02-07 15:21:52 -0500

    minor fix submit again

--- Hash_code_self_driving/Solver.py
+++ Hash_code_self_driving/Solver.py
@@ -60,7 +60,7 @@ class Solver:
         f = open(file_name,'w')
         for i in range(self.problem.F):
             f.write("%d " % len(self.vehicule_rides[i]))
-            f.writelines(["%d" % item  for item in self.vehicule_rides[i]])
+            f.writelines(["%d " % item  for item in self.vehicule_rides[i]])
             f.write("\n")
         f.close()
 
