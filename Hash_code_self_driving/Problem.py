bad206477f5432306d52324b4d881217a156e461
JustinHogue <justin.hogue.2@ulaval.ca>
2021-02-07 14:49:17 -0500

    Création de la classe Problem pour lire et décoder le problème et les fichiers.

--- Hash_code_self_driving/Problem.py
+++ Hash_code_self_driving/Problem.py
@@ -1,14 +1,34 @@
 class Problem:
     @staticmethod
     def get_from_file(filename: str):
-        pass
+        with open(filename, 'r') as file:
+            first_line = file.readline().split()
+            number_of_rows = int(first_line[0])
+            number_of_columns = int(first_line[1])
+            number_of_vehicules_in_fleet = int(first_line[2])
+            number_of_rides = int(first_line[3])
+            bonus = int(first_line[4])
+            number_of_steps = int(first_line[5])
+            rides = [] #Contient toutes les rides et leurs valeurs associées
+            for line in file.readlines()[1:]:
+                a = int(line[0])
+                b = int(line[1])
+                x = int(line[2])
+                y = int(line[3])
+                s = int(line[4])
+                f = int(line[5])
+                rides += (a, b, x, y, s, f)
+        return Problem(number_of_rows,number_of_columns,number_of_vehicules_in_fleet,number_of_rides,bonus,number_of_steps,rides)
 
-    def __init__(self):
-        self.R = 0
-        self.C = 0
-        self.F = 0
-        self.N = 0
-        self.B = 0
-        self.T = 0
 
-        self.rides = [] # [(a, b, x, y, s, f)]
+
+
+    def __init__(self,number_of_rows,number_of_columns,number_of_vehicules_in_fleet,number_of_rides,bonus,number_of_steps,rides):
+        self.R = number_of_rows
+        self.C = number_of_columns
+        self.F = number_of_vehicules_in_fleet
+        self.N = number_of_rides
+        self.B = bonus
+        self.T = number_of_steps
+
+        self.rides = rides #[(a, b, x, y, s, f)]
