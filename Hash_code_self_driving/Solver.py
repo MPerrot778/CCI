93163d651fbaf4d1866dd165682d4b3d29b72ce9
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2021-02-07 15:15:22 -0500

    fixed score

--- Hash_code_self_driving/Solver.py
+++ Hash_code_self_driving/Solver.py
@@ -17,7 +17,7 @@ class Solver:
         if current_step + distance_from_ride < ride[4]:
             score += self.problem.B
 
-        ride_duration = self.get_distance((ride[0], ride[1]), (ride[2], ride[3]))
+        ride_duration = self.get_distance((ride[0], ride[1]), (ride[2], ride[3])) + ride[4]
         if(current_step + ride_duration) > ride[5]:
             return None, 0
 
