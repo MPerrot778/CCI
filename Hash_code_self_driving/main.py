466f89d35c5062d4be07f3142838dd756519258c
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2021-02-07 14:53:25 -0500

    Fixed main

--- Hash_code_self_driving/main.py
+++ Hash_code_self_driving/main.py
@@ -2,7 +2,7 @@ from Problem import Problem
 from Solver import Solver
 
 if __name__ == '__main__':
-    problem = Problem.get_from_file("a_example.in")
+    problem = Problem.get_from_file("Problems/a_example.in")
     solver = Solver(problem)
     solver.solve()
     print(solver.vehicule_rides)
