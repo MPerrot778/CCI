06171908b484116b734ca7accc5f4cae33cf5574
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2021-02-07 14:52:19 -0500

    Added main

---
+++ Hash_code_self_driving/main.py
@@ -0,0 +1,8 @@
+from Problem import Problem
+from Solver import Solver
+
+if __name__ == '__main__':
+    problem = Problem.get_from_file("a_example.in")
+    solver = Solver(problem)
+    solver.solve()
+    print(solver.vehicule_rides)
