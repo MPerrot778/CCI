224f0ce060ae3faac118eb84f1b61311fa24f720
Frédéric Fortier-Chouinard <freud.fortier.chouinard@gmail.com>
2021-02-07 15:15:13 -0500

    updated main

--- Hash_code_self_driving/main.py
+++ Hash_code_self_driving/main.py
@@ -2,7 +2,8 @@ from Problem import Problem
 from Solver import Solver
 
 if __name__ == '__main__':
-    problem = Problem.get_from_file("Problems/a_example.in")
+    problem = Problem.get_from_file("Problems/b_should_be_easy.in")
     solver = Solver(problem)
     solver.solve()
+    solver.submit("b.out")
     print(solver.vehicule_rides)
